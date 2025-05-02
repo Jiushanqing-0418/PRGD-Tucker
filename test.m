ffca47f652963d839b448346f7afd0bc32b245ba
Blizzard_smith <75349235+Jiushanqing-0418@users.noreply.github.com>
2025-05-02 14:28:09 +0800

    Update test.m

--- test.m
+++ test.m
@@ -5,7 +5,7 @@ clear; clc;
 randn('state',27);
 rand('state',27);
 
-% Parameters
+% Parameters 
 d = 3;          % tensor order
 n = 100;        % dimension
 r = 5;          % rank
@@ -13,7 +13,7 @@ OS = 10;        % oversampling ratio
 nn = n * ones(1, d);
 rr = r * ones(1, d);
 
-% Sampling parameters
+% Sampling parameters 
 dims = r^d + d*(n*r - r*r);
 sizeOmega = min(OS*dims, 0.99*prod(nn));
 rho = sizeOmega / prod(nn);
@@ -25,11 +25,11 @@ X_GT = TUCKER_rand(rr, nn, d);
 X_GT_full = ttm(X_GT.core, X_GT.U, 1:d);
 X_GT_Omega = X_GT_full(Idx_Omega);
 
-% Initialization
+% Initialization 
 X0 = Spectral_initial_offdiag(X_GT_Omega, Idx_Omega, rr, nn, d, rho);
 opts_tucker = struct('maxiter', 10000, 'tol', 1e-8, 'rho', rho, 'alpha', 0);
 
-%% Algorithm Comparisons
+%% Algorithm Comparisons 
 % RGD
 opts_tucker.alpha = 3.70;
 [X_RGD, ~, GT_error_RGD, timer_RGD] = RGD(X_GT_full, X0, X_GT_Omega, Idx_Omega, opts_tucker);
@@ -46,13 +46,13 @@ opts_tucker.alpha = 0.135;
 fprintf('Runtimes (s):\nRGD: %.2f\nPRGD: %.2f\nScaledGD: %.2f\n', ...
         sum(timer_RGD), sum(timer_PRGD), sum(timer_ScaledGD));
 
-%% Visualizations
+%% Visualizations 
 close all
 colors = [0.85 0.325 0.098;  % Orange
           0.601 0.401 0.631;   % Purple
           0 0.447 0.741];  % Blue
 
-% Error vs Iterations
+% Error vs Iterations 
 figure(1);
 semilogy(GT_error_PRGD, '-', 'color', colors(1,:), 'linewidth', 2); hold on;
 semilogy(GT_error_ScaledGD, '-', 'color', colors(2,:), 'linewidth', 2);
@@ -62,7 +62,7 @@ xlabel('Iterations'); ylabel('Relative Error');
 legend({'PRGD', 'ScaledGD', 'RGD'});
 title(sprintf('OS = %d, d = %d, n = %d, r = %d', OS, d, n, r));
 
-% Error vs CPU Time
+% Error vs CPU Time 
 figure(2);
 semilogy(cumsum(timer_PRGD), GT_error_PRGD, '-', 'color', colors(1,:), 'linewidth', 2); hold on;
 semilogy(cumsum(timer_ScaledGD), GT_error_ScaledGD, '-', 'color', colors(2,:), 'linewidth', 2);
@@ -72,7 +72,7 @@ xlabel('CPU time'); ylabel('Relative Error');
 legend({'PRGD', 'ScaledGD', 'RGD'});
 title(sprintf('OS = %d, d = %d, n = %d, r = %d', OS, d, n, r));
 
-%% Functions
+%% Functions 
 function X = TUCKER_rand(rr, nn, d)
     X.core = tensor(rand(rr));
     X.U = cell(1, d);
